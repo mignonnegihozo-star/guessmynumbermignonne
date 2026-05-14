b5d59e5b9959230933fc3bb709a3c410d1247314
mignonnegihozo-star <mignonne.gihozo@stu.ocsb.ca>
2026-05-14 13:26:50 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+let guess_of_number = 0
+game.splash("Guess my number")
+let random = game.askForNumber("What number is your guess? (1 to 6)")
+if (guess_of_number < 6) {
+    game.splash("Please enter a valid number")
+    game.reset()
+} else if (false) {
+	
+}
